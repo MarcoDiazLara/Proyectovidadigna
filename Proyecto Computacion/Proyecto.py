d24d4d692898ab9753722fa55e65c3f03d7c6d8a
Marco Antonio Diaz Lara <70719501+MarcoDiazLara@users.noreply.github.com>
2022-02-09 12:46:37 -0600

    Add files via upload

--- Proyecto Computacion/Proyecto.py
+++ Proyecto Computacion/Proyecto.py
@@ -1,93 +1,8 @@
-import random
 import matematicas as mat
+import memorama as mem
 from tkinter import *
 from tkinter import ttk
-
-
-def suma():
-    llave_suma = True
-    entrada = True
-    contador = 0
-    cota_sup = num_A
-    while(entrada):
-        llave_suma = True
-        while(llave_suma):
-            A = random.randint(0,cota_sup)
-            B = random.randint(0,cota_sup)
-        
-            if((A+B)<cota_sup):
-                llave_suma = False
-            
-        print(A,"+",B,"=")
-        res = int(input())
-    
-        if(res == (A+B)):
-            contador += 1
-            print("Felicidades, Acertaste!!!!")
-            if(contador == 1):
-                print("Haz, acertado: ",contador,"suma")
-            else:
-                print("Haz, acertado: ",contador,"sumas")
-            
-        else:
-            print("Muy mal, esa no es la respuesta")
-            entrada = False
-        
-def resta():
-    llave_resta = True
-    entrada = True
-    contador = 0
-    cota_sup = num_A
-    while(entrada):
-        llave_resta = True
-        while(llave_resta):
-            A = random.randint(0,cota_sup)
-            B = random.randint(0,cota_sup)
-            
-            if(((A-B)<cota_sup) & ((A-B)>=0)):
-                llave_resta = False
-        print(A,"-",B,"=")
-        res = int(input())
-        
-        if(res == A-B):
-            contador += 1
-            print("Felicidades, Acertaste!!!!")
-            if(contador == 1):
-                print("Haz acertado: ",contador,"resta")
-            else:
-                print("Haz acertado: ",contador,"restas")
-        else:
-            print("Muy mal, esa no es la respuesta")
-            entrada = False
-
-def multi():
-    entrada = True
-    llave_multi = True
-    contador = 0
-    cota_sup = num_A
-    while(entrada):
-        llave_multi = True
-        while(llave_multi):
-            A = random.randint(1,cota_sup)
-            B = random.randint(1,cota_sup)
-            
-            if((A*B)<=cota_sup):
-                llave_multi = False
-        print(A,"*",B,"=")
-        res = int(input())
-        
-        if(res == A*B):
-            contador += 1
-            print("Felicidades, Acertaste!!!!")
-            if(contador == 1):
-                print("Haz acertado: ",contador,"multiplicacion")
-            else:
-                print("Haz acertado: ",contador,"multiplicaciones")
-        else:
-            print("Muy mal, esa no es la respuesta")
-            entrada = False
-
-    
+  
 
 def Salir():
     root.destroy()
@@ -109,10 +24,10 @@ fondo = Label(root, image = imagen, bg = "#26867d" ).place(x = 400, y = 200)
 B_Operaciones = Button(root,text="Repaso Matematico", font = ("toms handwritten new",18), bg = "#ef7f74", command = lambda: mat.pantalla_Repaso(root))
 B_Operaciones.place(x=200 , y=400, width=170, height=70)
 #Boton Resta
-B_Memorama = Button(root,text="Memorama", font = ("toms handwritten new",18), bg = "#ef7f74", command = Salir)
+B_Memorama = Button(root,text="Memorama", font = ("toms handwritten new",18), bg = "#ef7f74", command = lambda: mem.main(root))
 B_Memorama.place(x = 450,y = 400, width=130, height=70)
 #Boton multiplicacion
-B_ahorcado = Button(root, text="Ahorcado",font = ("toms handwritten new",18), bg = "#ef7f74", command= multi)
+B_ahorcado = Button(root, text="Ahorcado",font = ("toms handwritten new",18), bg = "#ef7f74", command= Salir)
 B_ahorcado.place(x = 650, y = 400, width=130, height=70)
 
 #Boton salida
